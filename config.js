b6a97bd273849a20060e307c59e06ee14f6bdc77
itgjz <itgjz@qq.com>
2019-11-02 17:45:32 +0800

    update ejs

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: '1111',
+    description: '46546465',
+
+
+    // blockchain-development
+    techPoint: [
+        {
+            title: 'Python',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/python.svg'
+        }, {
+            title: 'C++',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/c-1.svg'
+        }, {
+            title: 'C#',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/c.svg'
+        }, {
+            title: 'Unity',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/unity.svg'
+        }, {
+            title: 'PostgreSQL',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/postgresql.svg'
+        }, {
+            title: 'React.js',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/reactjs.svg'
+        }, {
+            title: 'Node.js',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/nodejs.svg'
+        }, {
+            title: 'Go',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/go.svg',
+        }, {
+            title: 'Solidity',
+            imgSrc: 'https://openledger.info/wp-content/uploads/2018/08/solidity.svg'
+        }
+    ]
+}
--- routes.js
+++ routes.js
@@ -1,84 +1,85 @@
 var express = require('express');
 var router = express.Router();
+var config = require('./config')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', config);
 });
 
 // Services
 router.get('/services', function (req, res, next) {
-  res.render('services/index')
+  res.render('services/index', config)
 })
 
 router.get('/services/blockchain-development', function (req, res, next) {
-  res.render('services/blockchain-development')
+  res.render('services/blockchain-development', config)
 })
 
 router.get('/services/eosio', function (req, res, next) {
-  res.render('services/eosio')
+  res.render('services/eosio', config)
 })
 
 router.get('/services/hyperledger', function (req, res, next) {
-  res.render('services/hyperledger')
+  res.render('services/hyperledger', config)
 })
 
 router.get('/services/research', function (req, res, next) {
-  res.render('services/research-and-development')
+  res.render('services/research-and-development', config)
 })
 
 router.get('/services/smart-contract', function (req, res, next) {
-  res.render('services/smart-contract')
+  res.render('services/smart-contract', config)
 })
 
 
 // Solutions
 router.get('/solutions/agriculture', function (req, res, next) {
-  res.render('solutions/agriculture')
+  res.render('solutions/agriculture', config)
 })
 
 router.get('/solutions/automotive', function (req, res, next) {
-  res.render('solutions/automotive')
+  res.render('solutions/automotive', config)
 })
 
 router.get('/solutions/charity', function (req, res, next) {
-  res.render('solutions/charity')
+  res.render('solutions/charity', config)
 })
 
 router.get('/solutions/education', function (req, res, next) {
-  res.render('solutions/education')
+  res.render('solutions/education', config)
 })
 
 router.get('/solutions/healthcare', function (req, res, next) {
-  res.render('solutions/healthcare')
+  res.render('solutions/healthcare', config)
 })
 
 router.get('/solutions/insurance', function (req, res, next) {
-  res.render('solutions/insurance')
+  res.render('solutions/insurance', config)
 })
 
 router.get('/solutions/ito', function (req, res, next) {
-  res.render('solutions/ito')
+  res.render('solutions/ito', config)
 })
 
 router.get('/solutions/legal', function (req, res, next) {
-  res.render('solutions/legal')
+  res.render('solutions/legal', config)
 })
 
 router.get('/solutions/logistics', function (req, res, next) {
-  res.render('solutions/logistics')
+  res.render('solutions/logistics', config)
 })
 
 router.get('/solutions/real-estate', function (req, res, next) {
-  res.render('solutions/real-estate')
+  res.render('solutions/real-estate', config)
 })
 
 router.get('/solutions/supply-chain', function (req, res, next) {
-  res.render('solutions/supply-chain')
+  res.render('solutions/supply-chain', config)
 })
 
 router.get('/solutions/retail', function (req, res, next) {
-  res.render('solutions/retail')
+  res.render('solutions/retail', config)
 })
 
 
