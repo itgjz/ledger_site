b912d4ef2c96bf198d3766f50c396bc6040ef6ae
itgjz <itgjz@qq.com>
2019-11-14 17:35:07 +0800

    翻译

--- data/services/smart-contract.js
+++ data/services/smart-contract.js
@@ -1,38 +1,38 @@
 module.exports = {
     SmartContractsForBusinesses: [
         {
-            title: '卫生保健',
-            description: '智能合约可以用来记录个人的医疗信息，医院之间的数据可由个人授权共享。也可以用于其它方面的管理包括对药品生产的监督、合法性、测试的结果等供应的管理等',
+            title: '医疗保健',
+            description: '通过智能合约可以用来记录个人的医疗信息和医院之间的数据，当然这些数据需要个人授权才能访问。也可以用于其它管理方面比如：对药品生产的监督、合法性、测试的结果和供应的管理等',
             imgSrc: '/images/Services/SmartContracts/SC1.png',
             imgSrcSet: '/images/Services/SmartContracts/SC1@2x.png',
             moreLink: '/solutions/healthcare'
         }, {
             title: '物流运输',
-            description: '智能合约能够使物流运输企业以粒度方式跟踪产品从制造商到商店的流程，简化了跟踪过程，并提供了整个供应链的实时可见性。',
+            description: '通过智能合约能够使物流运输企业以粒度方式跟踪产品从制造商到商店的整个完整流程，智能合约简化了跟踪的流程，并提供了整个供应过程实时的可见性和数据的准确性。',
             imgSrc: '/images/Services/SmartContracts/SC2.png',
             imgSrcSet: '/images/Services/SmartContracts/SC2@2x.png',
             moreLink: '/solutions/logistics'
         }, {
-            title: '房地产',
-            description: '智能合约可简化房地产行业中交易领域（例如租赁，购买和销售）的管理操作。该技术减轻了文字工作的负担和财务成本',
+            title: '不动产',
+            description: '智能合约可以自动化不动产行业中的交易（例如租赁，购买和出售）的管理操作，并且减轻了文字工作带来的负担和人员的成本还保证了交易的公平。',
             imgSrc: '/images/Services/SmartContracts/SC3.png',
             imgSrcSet: '/images/Services/SmartContracts/SC3@2x.png',
             moreLink: '/solutions/real-estate'
         }, {
             title: '汽车',
-            description: '在汽车工业中，智能合约用于保险程序。合约是定制的，包括真实的车辆性能信息。也可以自动执行保险金的收取和保险索赔后的赔付。',
+            description: '在汽车行业中，智能合约可用于记录并追踪车辆每个零部件的生产流程，基于区块链技术可以保证这些信息都是真实的。',
             imgSrc: '/images/Services/SmartContracts/SC4.png',
             imgSrcSet: '/images/Services/SmartContracts/SC4@2x.png',
             moreLink: '/solutions/automotive'
         }, {
             title: '保险',
-            description: '智能合约可以自动处理保单。他们可以根据合约中存储的索赔信息执行准确判断并计算出详细的支付金额自动进行赔付。',
+            description: '智能合约可以自动处理保单，根据合约中存储的索赔信息执行准确的判断并计算出详细的支付金额自动的进行赔付。',
             imgSrc: '/images/Services/SmartContracts/SC5.png',
             imgSrcSet: '/images/Services/SmartContracts/SC5@2x.png',
             moreLink: '/solutions/insurance'
         }, {
             title: '金融',
-            description: '金融机构一般使用以太坊的智能合约，利用区块链技术来改善数据质量，加强对投资者权益的保护并增强金融市场的功能。',
+            description: '金融机构一般使用Ethereum(以太坊)的智能合约，利用区块链技术来改善数据的质量，加强对投资者权益的保护并增强金融市场的功能。',
             imgSrc: '/images/Services/SmartContracts/SC6.png',
             imgSrcSet: '/images/Services/SmartContracts/SC6@2x.png',
             moreLink: '#'
