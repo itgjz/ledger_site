b0008ffc3b449b984bbf77c59b6b642014ec723d
itgjz <itgjz@qq.com>
2020-01-16 17:45:27 +0800

    transaction

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,6 @@
-(function ($) {
-    var location = window.location
-    if (location.origin.indexOf('https') === -1) {
-        location.href = 'https://' + location.host + location.pathname
-    }
-})(jQuery)
+// (function ($) {
+//     var location = window.location
+//     if (location.origin.indexOf('https') === -1) {
+//         location.href = 'https://' + location.host + location.pathname
+//     }
+// })(jQuery)
